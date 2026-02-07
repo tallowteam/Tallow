b43a4cf1e7c8649d44aa1a303e8902ebdd9faaac
Tallow <260103887+tallowteam@users.noreply.github.com>
2026-02-07 12:57:06 -0600

    auto-sync: 2026-02-07 12:57:05

--- components/landing/index.ts
+++ components/landing/index.ts
@@ -1,8 +1,8 @@
 export { Hero } from './Hero';
-export { default as Marquee } from './Marquee';
-export { default as FeatureBlock } from './FeatureBlock';
+export { Marquee } from './Marquee';
+export { FeatureBlock } from './FeatureBlock';
 export { TransferVisual, SecurityVisual, PlatformVisual } from './FeatureVisuals';
-export { default as HowItWorksPreview } from './HowItWorksPreview';
-export { default as PullQuote } from './PullQuote';
-export { default as Stats } from './Stats';
-export { default as CTA } from './CTA';
+export { HowItWorksPreview } from './HowItWorksPreview';
+export { PullQuote } from './PullQuote';
+export { Stats } from './Stats';
+export { CTA } from './CTA';
